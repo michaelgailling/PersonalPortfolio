6f2ef0f8534b34d7cd74724034fabe5128da0d45
Michael Gailling <michael.gailling@gmail.com>
2020-10-22 21:53:09 -0400

    Fixed typo in register page changed db uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    'URI': 'mongodb://localhost/contacts'
+    //'URI': 'mongodb://localhost/contacts'
+    'URI':"mongodb+srv://mike:ssvMmGjt3IaFGsoG@glider.tahrd.mongodb.net/Personal-Portfolio?retryWrites=true&w=majority"
 }
